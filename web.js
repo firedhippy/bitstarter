4f3c845da7bf2f745682e992704c384ccf1fedd2
firedhippy <firedhippy@gmail.com>
2013-07-12 18:36:41 +0000

    pushing web.js again

--- web.js
+++ web.js
@@ -6,7 +6,11 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 
-var text_from_index = fs.readFile(index.html);
+var text_from_index;
+fs.readFile('index.html',function(err,data) {
+	if(err) throw err;
+	text_from_index = data;
+});
 
 app.get('/', function(request, response) {
   response.send('text_from_index');
