e8fafcc8fc9b8bec8fc552e75ee1fee660025771
firedhippy <firedhippy@gmail.com>
2013-07-12 18:19:56 +0000

    added to web.js to write from index.html

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
+
+
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+
+var text_from_index = fs.readFile(index.html);
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send('text_from_index');
 });
 
 var port = process.env.PORT || 5000;
